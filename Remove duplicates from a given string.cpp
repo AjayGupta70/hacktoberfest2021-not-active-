96734953bb2b01c8f85f2bb23463186f452280ea
AjayGupta70 <110338153+AjayGupta70@users.noreply.github.com>
2022-10-31 20:27:03 +0530

    Create Remove duplicates from a given string.cpp

---
+++ Remove duplicates from a given string.cpp
@@ -0,0 +1,28 @@
+// CPP program to remove duplicate character
+// from character array and print in sorted
+// order
+#include <bits/stdc++.h>
+using namespace std;
+
+char *removeDuplicate(char str[], int n)
+{
+	
+	unordered_set<char>s (str, str+n-1);
+
+
+	int i = 0;
+	for (auto x : s)
+	str[i++] = x;
+	str[i] = '\0';
+
+	return str;
+}
+
+
+int main()
+{
+char str[]= "geeksforgeeks";
+int n = sizeof(str) / sizeof(str[0]);
+cout << removeDuplicate(str, n);
+return 0;
+}
